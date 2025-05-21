4403d2a94a6fcd6b6a0a98d4a0625399d30f569b
Mr_Rahul <dipbag009@gmail.com>
2025-05-21 21:59:34 +0530

    add unit selector box and use javascript

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
-    const btn = document.getElementById('menu-btn');
+ const btn = document.getElementById('menu-btn');
     const menu = document.getElementById('mobile-menu');
 
     btn.addEventListener('click', () => {
-        const isOpen = menu.classList.contains('max-h-0');
-        if (isOpen) {
-            menu.classList.remove('max-h-0', 'opacity-0');
-            menu.classList.add('max-h-96', 'opacity-100');
-        } else {
-            menu.classList.remove('max-h-96', 'opacity-100');
-            menu.classList.add('max-h-0', 'opacity-0');
-        }
+      const isOpen = menu.classList.contains('max-h-96');
+      if (isOpen) {
+        menu.classList.remove('max-h-96', 'opacity-100');
+        menu.classList.add('max-h-0', 'opacity-0');
+      } else {
+        menu.classList.remove('max-h-0', 'opacity-0');
+        menu.classList.add('max-h-96', 'opacity-100');
+      }
     });
+   function print() {
+     let inputPrint1 = document.getElementById("input-1").value;
+    let inputPrint2 = document.getElementById("input-2").value;
 
+   let print = document.getElementById("printUnits");
+   print.innerText = inputPrint1 +" To "+inputPrint2;
+   chack();
+   }
+   function chack() {
+    let inputPrint1 = document.getElementById("input-1").value;
+    let inputPrint2 = document.getElementById("input-2").value;
+
+   let print = document.getElementById("printUnits");
+   if (inputPrint1 == inputPrint2) {
+    print.innerText = "Please select deferent unit's";
+   }
+   else if (inputPrint1 != inputPrint2) {
+   calculate();
+   }
+   else {
+    print.innerText = "Please select an unit's";
+   }
+   }
+    function calculate() {
+      
+    }
